bebd932e88e2994308f30862cbec5d4ab37ea909
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-11-30 17:44:55 -0600

    okay for real this time I got everything set up proper with intellij, was way harder than it needed to be

--- coronavirus-tracker/src/main/java/io/benjamin/coronavirustracker/CoronavirusTrackerApplication.java
+++ coronavirus-tracker/src/main/java/io/benjamin/coronavirustracker/CoronavirusTrackerApplication.java
@@ -1,4 +1,4 @@
-package io.javabrains.coronavirustracker;
+package io.benjamin.coronavirustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoronavirusTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoronavirusTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoronavirusTrackerApplication.class, args);
+    }
 
 }
--- coronavirus-tracker/src/test/java/io/benjamin/coronavirustracker/CoronavirusTrackerApplicationTests.java
+++ coronavirus-tracker/src/test/java/io/benjamin/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package io.javabrains.coronavirustracker;
+package io.benjamin.coronavirustracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CoronavirusTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
